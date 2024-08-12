2eb26b831407955d65b87955bd2cd6b3e125e437
amirkh067 <90905503+amirkh067@users.noreply.github.com>
2024-08-12 18:12:35 +0300

    Add files via upload

---
+++ ListInstalledSoftware.ps1
@@ -0,0 +1,7 @@
+# ListInstalledSoftware.ps1
+
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | 
+Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | 
+Where-Object { $_.DisplayName } | 
+Sort-Object DisplayName |
+Format-Table -AutoSize
